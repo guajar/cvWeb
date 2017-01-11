ff8ff17d400dcd00f868ad15151797a65369d3de
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-01-11 23:47:28 +0100

    Added Contact section with form

--- js/form.js
+++ js/form.js
@@ -1,35 +1,29 @@
 var form = document.getElementById("form-contact");
 
-var apellidosInput = document.getElementsByName("tienes_apellidos");
-var ejercitoInput = document.getElementById("ejercito");
-var tooManyEnemies = document.getElementById("too-many-enemies");
+var tel = document.getElementById("telefono");
+var howto = document.getElementsByName("tipo_howto");
 
-var inputApellidos = document.createElement("input");
-inputApellidos.setAttribute("id", "apellidos");
-inputApellidos.setAttribute("type", "text");
-inputApellidos.setAttribute("name", "apellidos");
-inputApellidos.setAttribute("placeholder", "Apellidos");
-inputApellidos.setAttribute("required", "");
+var otros;
 
-for (var i = 0; i < apellidosInput.length; i++) {
-    apellidosInput[i].addEventListener('click', function(event){
-        if (this.value == 'yes') {
-            this.parentNode.appendChild(inputApellidos);    
+var inputOtros = document.createElement("input");
+inputOtros.setAttribute("id", "input_otros");
+inputOtros.setAttribute("class", "other");
+inputOtros.setAttribute("type", "text");
+inputOtros.setAttribute("name", "otros");
+inputOtros.setAttribute("value", otros);
+inputOtros.setAttribute("placeholder", "¿Cómo me conociste?");
+
+for (var i = 0; i < howto.length; i++) {
+    howto[i].addEventListener('click', function(event){
+        if (this.value == 'Otros') {
+            this.parentNode.appendChild(inputOtros);    
         } else {
-            if(document.getElementById("apellidos")) {
-                this.parentNode.removeChild(inputApellidos);
+            if(document.getElementById("input_otros")) {
+                this.parentNode.removeChild(inputOtros);
             }
         }
     });
 }
-
-ejercitoInput.addEventListener("keyup", function(event){
-    if(this.value > 50) {
-        tooManyEnemies.style.display = 'block';
-    } else {
-        tooManyEnemies.style.display = 'none';
-    }
-});
 
 form.addEventListener("submit", function(event){
     var inputNombre = document.getElementById("nombre");

22a0a361750668ae8f87733c14a153a61411a2aa
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-01-14 14:02:27 +0100

    Added Validity at Form and removed number-polyfill.js

--- js/form.js
+++ js/form.js
@@ -1,73 +1,67 @@
 var form = document.getElementById("form-contact");
 
 var tel = document.getElementById("telefono");
-var howto = document.getElementsByName("tipo_howto");
-
-var otros;
+var valSelector = document.getElementById("otherSelector");
 
 var inputOtros = document.createElement("input");
 inputOtros.setAttribute("id", "input_otros");
 inputOtros.setAttribute("class", "other");
 inputOtros.setAttribute("type", "text");
 inputOtros.setAttribute("name", "otros");
-inputOtros.setAttribute("value", otros);
-inputOtros.setAttribute("placeholder", "¿Cómo me conociste?");
+inputOtros.setAttribute("placeholder", "Introduce a través de que o quién...");
+inputOtros.setAttribute("required", "");
 
-for (var i = 0; i < howto.length; i++) {
-    howto[i].addEventListener('click', function(event){
-        if (this.value == 'Otros') {
-            this.parentNode.appendChild(inputOtros);    
-        } else {
-            if(document.getElementById("input_otros")) {
-                this.parentNode.removeChild(inputOtros);
-            }
+$(document).ready(function() {
+    $("#telefono").keydown(function (e) {
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+            (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||              
+            (e.keyCode >= 35 && e.keyCode <= 40)) {
+                 return;
+        }
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
         }
     });
-}
+});
+
+$(document).ready(function(){
+  $("#otherSelector").change(function(){
+    $("#otherSelector option:selected").each(function() {			
+        if (this.id == "tipo_otros") {
+           $('#divSel').append(inputOtros);    
+        } else {                  
+            $('#input_otros').remove();
+        }			
+    }); 
+  });		 	
+});
+
+$(document).ready(function() {
+    $('#comment').on('keyup', function() {
+        var words = this.value.match(/\S+/g).length;  
+        if (words > 150) {
+            var trimmed = $(this).val().split(/\s+/, 150).join(" ");
+            $(this).val(trimmed + " ");
+        }
+        else {
+            $('#display_count').text(words);
+            $('#word_left').text(150-words);
+        }
+    });
+ }); 
 
 form.addEventListener("submit", function(event){
-    var inputNombre = document.getElementById("nombre");
-    var apellidosRadioInput = {
-        "apellidos_si": document.getElementById("apellidos_si"),
-        "apellidos_no": document.getElementById("apellidos_no")
-    };
+    var inputNombre = document.getElementById("nombre");    
     var emailInput = document.getElementById("email");
-
-    var misionesRadioInput = {
-        "mision1": document.getElementById("tipo_mision_1"),
-        "mision2": document.getElementById("tipo_mision_2"),
-        "mision3": document.getElementById("tipo_mision_3"),
-        "mision4": document.getElementById("tipo_mision_4"),
-    };
-
-    var estasSeguroRadioInput = {
-        "seguro_si": document.getElementById("seguro_si"),
-        "seguro_no": document.getElementById("seguro_no")
-    };
-
-    var fechaInput = document.getElementById("fecha");
-    var submitInput = document.getElementById("enviar");
+    var telef = document.getElementById("telefono");
+    var selHowto = document.getElementById("otherSelector");
+    var submitInput = document.getElementById("enviar");    
 
     if(inputNombre.checkValidity() == false) {
         alert("Escribe tu nombre");
         inputNombre.focus();
         event.preventDefault();
         return false;
-    }
-
-    if(apellidosRadioInput.apellidos_si.checkValidity() == false) {
-        alert("Selecciona si tienes apellidos");
-        event.preventDefault();
-        return false;
-    }
-
-    if(document.getElementById("apellidos")) {
-        if(document.getElementById("apellidos").checkValidity() == false){
-            alert("Escribe tus apellidos");
-            document.getElementById("apellidos").focus();
-            event.preventDefault();
-            return false;
-        }
     }
 
     if(email.checkValidity() == false) {
@@ -77,25 +71,28 @@ form.addEventListener("submit", function(event){
         return false;
     }
 
-    if(misionesRadioInput.mision1.checkValidity() == false) {
-        alert("Introduce el tipo de mision");
+    if(telef.checkValidity() == false) {
+        alert("Escribe el teléfono correcto");
+        telef.focus();
         event.preventDefault();
         return false;
     }
 
-    if(tooManyEnemies.style.display === 'block') {
-        if(estasSeguroRadioInput.seguro_si.checkValidity() == false){
-            alert("Confirmanos que estás seguro");
+    var index = selHowto.selectedIndex;
+    if( index == null || index == 0 ) {
+        alert("Escribe selección válida");
+        selHowto.focus();
+        event.preventDefault();
+        return false;
+    }
+
+    if(document.getElementById("input_otros")) {
+        if(document.getElementById("input_otros").checkValidity() == false){
+            alert("Escribe como me conociste");
+            document.getElementById("input_otros").focus();
             event.preventDefault();
             return false;
         }
-    }
-
-    if (fechaInput.checkValidity() == false) {
-        alert("Introduce la fecha de la mision");
-        fecha.focus();
-        event.preventDefault();
-        return false;
     }
 
     event.preventDefault();
---
+++ js/send-notification.js
@@ -0,0 +1,18 @@
+function sendNotification(msg, body) {
+	var notification = Notification || mozNotification || webkitNotification;
+	if (body) {
+		var options = {
+			body: body			
+		};
+	}
+
+	if (typeof(notification) === 'undefined') {
+		console.log("Web notification not supported");
+	} else {
+		notification.requestPermission(function(permission) {
+			if (Notification.permission === 'granted') {
+				var notification = new Notification(msg, body);
+			}
+		});
+	}
+}
